9bc8ba85934e9fed93976376eab09af8710de42a
Mehnaz Ahmed <mehnaz.ahmed225@gmail.com>
2026-07-24 09:17:36 -0400

    Refactor fasta_parser to include descriptions and IDs
    Updated fasta_parser to store sequence descriptions and sequences in a list. Adjusted how sequence IDs are generated.

--- BaseC/ExonScorer/fasta_parser.py
+++ BaseC/ExonScorer/fasta_parser.py
@@ -1,19 +1,23 @@
 """
 FASTA PARSER NOTES:
 
-The Fasta Parser returns a dictionary from the fasta file. I'll need to figure out how to make it easier to identify the different sequences in a Fasta File
+When using the Fasta File Parser, the parser returns current ID as the keys and the name of the fasta entry as the description which is index zero of the list. 
+The index 1 of the list is the actual sequence in the entry. 
 
 """
 
 def fasta_parser(filepath):
     sequences = {}
     with open(filepath, "r") as f:
+        seq = 1
         for line in f:
             line = line.strip()
             if line.startswith(">"):
-                current_id = line[1:]
-                sequences[current_id] = ""
+                current_description = line[1:]
+                current_id = f"SEQ_{seq}"
+                seq+=1
+                sequences[current_id] = [current_description, ""]
             elif current_id is not None:
-                sequences[current_id] += line
+                sequences[current_id][1] += line
 
     return sequences
